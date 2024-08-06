f97a3e8eeaf8a5c366d16f9271bf6bea123021f0
Mark Cottman-Fields <mark.cottmanfields@qcif.edu.au>
2024-08-07 09:26:37 +1000

    another try to run in circleci

--- support/prepare-host.sh
+++ support/prepare-host.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-UID=1000
-GID=1000
+DOCKER_UID=1000
+DOCKER_GID=1000
 
 SOLR_UID=8983
 SOLR_GID=8983
@@ -20,7 +20,7 @@ SUPPORT_DIR="$(pwd)/support"
 # Create directories.
 TMP_DIR="${SUPPORT_DIR}/.tmp"
 sudo mkdir -p "${TMP_DIR}"
-sudo chown "${UID}:${GID}" "${TMP_DIR}"
+sudo chown "${DOCKER_UID}:${DOCKER_GID}" "${TMP_DIR}"
 setupDone "${TMP_DIR}"
 
 SOLR_DIR="${TMP_DIR}/solr"
@@ -30,34 +30,34 @@ setupDone "${SOLR_DIR}"
 
 MONGO_DIR="${TMP_DIR}/mongo"
 sudo mkdir -p "${MONGO_DIR}"
-sudo chown "${UID}:${GID}" "${MONGO_DIR}"
+sudo chown "${DOCKER_UID}:${DOCKER_GID}" "${MONGO_DIR}"
 setupDone "${MONGO_DIR}"
 
 SAILS_DIR="${TMP_DIR}/sails"
 sudo mkdir -p "${SAILS_DIR}"
-sudo chown "${UID}:${GID}" "${SAILS_DIR}"
+sudo chown "${DOCKER_UID}:${DOCKER_GID}" "${SAILS_DIR}"
 setupDone "${SAILS_DIR}"
 
 SAILS_WORKING_DIR="${TMP_DIR}/sailsWorkingDir/tmp"
 sudo mkdir -p "${SAILS_WORKING_DIR}";
-sudo chown -R "${UID}:${GID}" "${SAILS_WORKING_DIR}"
+sudo chown -R "${DOCKER_UID}:${DOCKER_GID}" "${SAILS_WORKING_DIR}"
 setupDone "${SAILS_WORKING_DIR}"
 
 ATTACHMENTS_DIR="${TMP_DIR}/attachments"
 sudo mkdir -p "${ATTACHMENTS_DIR}"
-sudo chown -R "${UID}:${GID}" "${ATTACHMENTS_DIR}"
+sudo chown -R "${DOCKER_UID}:${DOCKER_GID}" "${ATTACHMENTS_DIR}"
 setupDone "${ATTACHMENTS_DIR}"
 
 ATTACHMENTS_STAGING_DIR="${ATTACHMENTS_DIR}/staging"
 sudo mkdir -p "${ATTACHMENTS_STAGING_DIR}"
-sudo chown -R "${UID}:${GID}" "${ATTACHMENTS_STAGING_DIR}"
+sudo chown -R "${DOCKER_UID}:${DOCKER_GID}" "${ATTACHMENTS_STAGING_DIR}"
 setupDone "${ATTACHMENTS_STAGING_DIR}"
 
 JUNIT_DIR="${TMP_DIR}/junit"
 sudo mkdir -p "${JUNIT_DIR}"
 sudo mkdir -p "${JUNIT_DIR}/backend-mocha"
 sudo mkdir -p "${JUNIT_DIR}/backend-bruno"
-sudo chown -R "${UID}:${GID}" "${JUNIT_DIR}"
+sudo chown -R "${DOCKER_UID}:${DOCKER_GID}" "${JUNIT_DIR}"
 setupDone "${JUNIT_DIR}"
 
 # Compile the typescript.
